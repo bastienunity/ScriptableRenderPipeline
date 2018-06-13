b02a77d23e7da1f7918d907243fb922551f822d3
Alex Lindman <alexandral@unity3d.com>
2018-06-13 14:56:59 -0700

    small sampling of wave form nodes

---
+++ com.unity.shadergraph/Editor/Data/Nodes/Math/Waves/NoiseWaveNode.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Reflection;
+
+namespace UnityEditor.ShaderGraph
+{
+    [Title("Math", "Wave", "Noise Wave")]
+    class NoiseWaveNode : CodeFunctionNode
+    {
+        public NoiseWaveNode()
+        {
+            name = "Noise Wave";
+        }
+
+        public override string documentationURL
+        {
+            get { return "https://github.com/Unity-Technologies/ShaderGraph/wiki/Noise-Wave-Node"; }
+        }
+
+        protected override MethodInfo GetFunctionToConvert()
+        {
+            return GetType().GetMethod("NoiseWave", BindingFlags.Static | BindingFlags.NonPublic);
+        }
+
+        static string NoiseWave(
+            [Slot(0, Binding.None)] DynamicDimensionVector In,
+            [Slot(1, Binding.None, 5f, 30f, 1, 1)] Vector2 Seed,
+            [Slot(3, Binding.None, -0.5f, 0.5f, 1, 1)] Vector2 MinMax,
+            [Slot(4, Binding.None)] out DynamicDimensionVector Out)
+        {
+            return
+                @"
+{
+    {precision} randomno =  frac(sin(dot(Seed, float2(12.9898, 78.233)))*43758.5453);
+    float noise = lerp(MinMax.x, MinMax.y, randomno);
+    Out = sin(In + noise);
+}
+";
+        }
+    }
+}
